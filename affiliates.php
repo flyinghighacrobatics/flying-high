9ad972cfd328f84ff7ecc8395871cfcc221bcf9f
Martin Kvist <martin@flyinghighacrobatics.com>
2016-02-01 18:38:37 +0100

    resposive bug debug

---
+++ affiliates.php
@@ -0,0 +1,151 @@
+<?php
+$pageTitle = 'Flying High Acrobatics - Affiliates';
+include('header.php');
+ ?>
+
+ <!-- Page Title -->
+ <div class="page-title-container">
+     <div class="container">
+         <div class="row">
+             <div class="col-sm-12 wow fadeIn">
+                 <i class="fa fa-user"></i>
+                 <h1>Affiliates, Partners & Resources /</h1>
+                 <p>Below you can find more information about our affiliates.</p>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- <div class="slider-container">
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-10 col-sm-offset-1 slider">
+                <h1>Partner Acrobatics Advanced Intensive</h1>
+                <h2>From May 20th to 29th in Andalusia, Spain</h2>
+                  <div class="flexslider">
+  <ul class="slides">
+ <li data-thumb="assets/img/slider/19.jpg">
+     <img src="assets/img/slider/19.jpg">
+     <div class="flex-caption">
+       Flying High in Andalusia ? <br> 9 days of Advanced Acrobatics Training with <span class='violet'>Partner Acrobatics</span> May 20th to 29th.
+     </div>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+
+     <!-- Presentation -->
+  <!--   <div class="presentation-container">
+       <div class="container">
+         <div class="row">
+           <div class="col-sm-12 wow fadeInLeftBig">
+             <h1><span class="violet">Flying High</span> Acrobatics...
+                                 </h1>
+             <p>Turning sparks of <span class='violet'>possibility</span> into flames of <span class='violet'>achievement</span>,
+                and building <span class='violet'>trust</span> all over the world...</p>
+
+             </div> -->
+       <!--      <div class="col-sm-12 wow fadeInLeftBig">
+
+       </div>-->
+
+               <!-- Meet Our Team -->
+               <div class="team-container">
+               	<div class="container">
+       	            <div class="row">
+       		            <div class="col-sm-12 team-title wow fadeIn">
+       		                <h2>Meet Our Affiliates</h2>
+       		            </div>
+       	            </div>
+       	            <div class="row">
+       	            	<div class="col-sm-3">
+       		                <div class="team-box wow fadeInUp">
+       		                    <img src="assets/img/team/1.jpg" alt="" data-at2x="assets/img/team/1.jpg">
+       		                    <h3>Partner Acrobatics</h3>
+       		                    <p><b> Partner Acrobatics offer two types of trainings:</b>
+       <br>
+       <ul>
+       <li> Teacher Training - Month long.
+       <li> Intensive Training - 1 or 2 weeks long.
+     </ul>
+       <br><br>
+       <b>More info at:</b>
+       <a href="http://partneracrobatics.com/">partneracrobatics.com</a>
+     </p>
+
+        	                    <div class="team-social">
+       		                        <a href="#"><i class="fa fa-facebook"></i></a>
+       		                        <!-- <a href="#"><i class="fa fa-twitter"></i></a>
+       		                        <a href="#"><i class="fa fa-linkedin"></i></a>
+       		                        <a href="#"><i class="fa fa-envelope"></i></a> -->
+       		                    </div>
+       		                </div>
+       	                </div>
+
+       	                <div class="col-sm-3">
+       		                <div class="team-box wow fadeInDown">
+       		                    <img src="assets/img/team/2.jpg" alt="" data-at2x="assets/img/team/2.jpg">
+       		                    <h3>One Minute Practice</h3>
+       		                    <p>The most natural way to solve our problems<br>
+                               Using just our breath to address them.<br><br>
+
+
+                                        <li><strong>Find out more at:</strong><br>
+                                        <a href="http://one-minute-practice.com/">one-minute-practice.com</a></li>
+                                     </ul>
+                                   </p>
+       		                </div>
+       	                </div>
+
+       	                <div class="col-sm-3">
+       		                <div class="team-box wow fadeInUp">
+       		                    <img src="assets/img/team/3.jpg" alt="" data-at2x="assets/img/team/3.jpg">
+       		                    <h3>The Maitri Movement</h3>
+       		                    <p><span class='violet'>Moving for freedom, joy and peace</span><br>
+                               Sharpen your senses, practice presence and break to tune in with the life force around and within!
+
+
+           <span class='violet' >In diversity, we find unity. Let's be the catalyst of our change.</span>
+
+           <br>
+           More info at:
+           <li><a href="http://www.themaitrimovement.com">www.themaitrimovement.com</a></li>
+           </p>
+       		                    <div class="team-social">
+       		                        <a href="#"><i class="fa fa-facebook"></i></a>
+       		                        <!-- <a href="#"><i class="fa fa-twitter"></i></a>
+       		                        <a href="#"><i class="fa fa-linkedin"></i></a>
+       		                        <a href="#"><i class="fa fa-envelope"></i></a> -->
+       		                    </div>
+       		                </div>
+       	                </div>
+
+       	                <div class="col-sm-3">
+       		                <div class="team-box wow fadeInDown">
+       		                    <img src="assets/img/team/4.jpg" alt="" data-at2x="assets/img/team/4.jpg">
+       		                    <h3>Mimi and Acro for the People</h3>
+       		                    <p><span class='violet'>Facilitating awesomeness worldwide...</span>
+       <br><br>
+         More info at:
+         <li><a href="http://www.acroforthepeople.com">www.acroforthepeople.com</a></li>
+                               </p>
+       		                    <!-- <div class="team-social">
+       		                        <a href="#"><i class="fa fa-facebook"></i></a>
+       		                        <a href="#"><i class="fa fa-twitter"></i></a>
+       		                        <a href="#"><i class="fa fa-linkedin"></i></a>
+       		                        <a href="#"><i class="fa fa-envelope"></i></a>
+       		                    </div> -->
+       		                </div>
+       	                </div>
+       	            </div>
+       	        </div>
+               </div>
+       <!-- End team container-->
+
+
+
+
+<?php
+include('footer.php');
+?>
---
+++ event-mindmov.php
@@ -0,0 +1,77 @@
+<?php
+$pageTitle = 'Flying High Acrobatics - Mindful Moves';
+include('header.php');
+ ?>
+ <div class="slider-container">
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-10 col-sm-offset-1 slider">
+                <h1>Mindful Moves</h1>
+                <h2>From May ....</h2>
+                  <div class="flexslider">
+  <ul class="slides">
+ <li data-thumb="assets/img/slider/19.jpg">
+     <img src="assets/img/slider/19.jpg">
+     <div class="flex-caption">
+       Are you moving or being moved? <br> ..... with <span class='violet'>The Maitri Movement</span> May ......
+     </div>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+     <!-- Presentation -->
+  <!--   <div class="presentation-container">
+       <div class="container">
+         <div class="row">
+           <div class="col-sm-12 wow fadeInLeftBig">
+             <h1><span class="violet">Flying High</span> Acrobatics...
+                                 </h1>
+             <p>Turning sparks of <span class='violet'>possibility</span> into flames of <span class='violet'>achievement</span>,
+                and building <span class='violet'>trust</span> all over the world...</p>
+
+             </div> -->
+       <!--      <div class="col-sm-12 wow fadeInLeftBig">
+
+       </div>-->
+       <div class="services-full-width-container">
+         <div class="container">
+             <div class="row">
+                 <div class="col-sm-12 services-full-width-text wow fadeInLeft">
+                     <h3>Take Your Training To The Next Level!</h3>
+                     <p>
+                       Join <span class='violet'>Flying High Acrobatics & The Maitri Movement</span> for
+                     </p>
+
+
+                   </div>
+                   </div>
+                   </div>
+                   </div>
+
+
+       <!-- Call To Action -->
+       <div class="call-to-action-container">
+         <div class="container">
+             <div class="row">
+                 <div class="col-sm-12 call-to-action-text wow fadeInLeftBig">
+                     <p>
+                       The event you have been waiting for is here! Together with Partner Acrobatics we present <span class="violet">Advanced Acrobatics Training</span>, nine days of flying high acrobatics
+                       at an amazing location in Andalusia, Spain.
+                     </p>
+                     <div class="call-to-action-button">
+                         <a class="big-link-3" href="#">Sign Up Now!</a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+       </div>
+
+
+
+
+<?php
+include('footer.php');
+?>
